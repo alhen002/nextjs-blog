818d1d79a4b8fca5e0af36757e1aecb085b5ee73
Alexander Henting <a.henting@posteo.de>
2024-01-11 10:15:31 +0100

    added suspense for image slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,7 +1,8 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { ArrowLeft, ArrowRight, Circle } from "lucide-react";
+import Loading from "@/app/loading";
 
 interface ImageSliderProps {
   images: string[];
@@ -30,13 +31,15 @@ function ImageSlider({ images, timeInterval = 3000, auto }: ImageSliderProps) {
 
   return (
     <div className="flex flex-col gap-1">
-      <Image
-        src={`/${images[currentImage]}`}
-        alt={images[currentImage].split(".")[0]}
-        width={765}
-        height={765}
-        className="grayscale rounded-lg"
-      />
+      <Suspense fallback={<Loading />}>
+        <Image
+          src={`/${images[currentImage]}`}
+          alt={images[currentImage].split(".")[0]}
+          width={765}
+          height={765}
+          className="grayscale rounded-lg"
+        />
+      </Suspense>
       <div className="flex gap-2 mx-auto w-12">
         {images.map((Image, index) => (
           <Circle
