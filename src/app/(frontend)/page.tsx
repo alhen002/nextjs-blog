c4e2a93261f77366c154bba99dd2386922fde09e
Alexander Henting <a.henting@posteo.de>
2024-01-15 13:25:59 +0100

    simple revalidation

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -9,7 +9,10 @@ import { GENERAL_QUERY } from "@/lib/sanity.queries";
 import { General } from "@/lib/types";
 import { CustomPortableText } from "@/components/backend/CustomPortableText";
 import { client } from "@/lib/sanity.client";
+
+//TODO: WORK ON PROPER REVALIDATION
 export const revalidate = 0;
+
 export default async function Home() {
   const general = await client.fetch<General>(GENERAL_QUERY);
 
