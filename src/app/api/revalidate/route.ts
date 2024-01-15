307945b47e16b8039d889c5fdd059c5143201546
Alexander Henting <a.henting@posteo.de>
2024-01-15 12:08:24 +0100

    next-try

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,18 +1,13 @@
-// api/revalidate/route.tsx
-
 import { revalidateTag } from "next/cache";
-import type { NextApiRequest } from "next";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
-
-export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
   try {
-    const { isValidSignature, body } = await parseBody<
-      BodyInit & { _type: string }
-    >(req as unknown as NextApiRequest, process.env.SANITY_REVALIDATE_SECRET);
+    const { isValidSignature, body } = await parseBody<{ _type: string }>(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET,
+    );
 
     if (!isValidSignature) {
       const message = "Invalid signature";
@@ -22,24 +17,14 @@ export async function POST(req: NextRequest) {
     }
 
     if (!body?._type) {
-      return new Response(body, { status: 400 });
+      revalidateTag("general");
+    } else {
+      revalidateTag(body?._type);
     }
 
-    // All `client.fetch` calls with `{next: {tags: [_type]}}` will be revalidated
-    revalidateTag(body._type);
-    console.log(`Revalidated ${body._type}`);
-
-    return NextResponse.json({
-      status: 200,
-      revalidated: true,
-      now: Date.now(),
-      body,
-    });
-  } catch (err: unknown) {
+    return NextResponse.json({ body });
+  } catch (err) {
     console.error(err);
-    if (err instanceof Error) {
-      return new Response(err.message, { status: 500 });
-    }
-    return new Response("Error", { status: 500 });
+    return new Response(JSON.stringify(err), { status: 500 });
   }
 }
