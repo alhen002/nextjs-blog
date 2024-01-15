38be0759b8cc44b2682a9a4882ac212a3ed71679
Alexander Henting <a.henting@posteo.de>
2024-01-15 12:16:03 +0100

    asdf

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -3,28 +3,5 @@ import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
 
 export async function POST(req: NextRequest) {
-  try {
-    const { isValidSignature, body } = await parseBody<{ _type: string }>(
-      req,
-      process.env.SANITY_REVALIDATE_SECRET,
-    );
-
-    if (!isValidSignature) {
-      const message = "Invalid signature";
-      return new Response(JSON.stringify({ message, isValidSignature, body }), {
-        status: 401,
-      });
-    }
-
-    if (!body?._type) {
-      revalidateTag("general");
-    } else {
-      revalidateTag(body?._type);
-    }
-
-    return NextResponse.json({ body });
-  } catch (err) {
-    console.error(err);
-    return new Response(JSON.stringify(err), { status: 500 });
-  }
+  revalidateTag("general");
 }
